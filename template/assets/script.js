bfd32876b7e446693af68023f0aca73aa04592d8
TomaMarenina <82373643+TomaMarenina@users.noreply.github.com>
2021-04-14 13:55:20 +0700

    Add files via upload

--- template/assets/script.js
+++ template/assets/script.js
@@ -10,6 +10,9 @@ $('.js-guess').click(function(){
 		$this.html('Загадано &#10004;');
 		$this.prop('disabled', true);
 		last_guess = JSON.parse(request);
+		for (key in last_guess) {
+			$('.js-psy-' + key).append('<div class="result__one">' + last_guess[key] + '</div>');
+		}
 		$('.js-right-answer').show();
 	})
 });
@@ -17,22 +20,24 @@ $('.js-guess').click(function(){
 // кнопка "отправить экстрасенсам свое загаданное число"
 $('.js-check').click(function(){
 	let value = $('.js-input').val();
-	$.ajax({
-		method: 'POST',
-		data: {'user_input': value},
-	}).done(function(request){
-		$('.js-guess').html('Загадать');
-		for (key in last_guess) {
-			$('.js-psy-' + key).append('<div class="result__one">' + last_guess[key] + '</div>');
-		}
-		let rely = JSON.parse(request);
-		for (key_r in rely['reliabilities']) {
-			$('.js-rely-' + key_r).html('<i>Рейтинг:<br />' + rely['reliabilities'][key_r] + '</i>');
-		}
-		$('.user-input').append('<div class="user-input__item">' + value + '</div>');
-		$('.js-guess').prop('disabled', false);
-		$('.js-right-answer').hide();
-		$('.js-input').val('')
-	})
+	if (value.match(/^[1-9][0-9]$/)) {
+		$.ajax({
+			method: 'POST',
+			data: {'user_input': value},
+		}).done(function(request){
+			$('.js-guess').html('Загадать');
+			
+			let rely = JSON.parse(request);
+			for (key_r in rely['reliabilities']) {
+				$('.js-rely-' + key_r).html('<i>Рейтинг:<br />' + rely['reliabilities'][key_r] + '</i>');
+			}
+			$('.user-input').append('<div class="user-input__item">' + value + '</div>');
+			$('.js-guess').prop('disabled', false);
+			$('.js-right-answer').hide();
+			$('.js-input').val('')
+		});
+	} else {
+		console.log('не двузначное число');
+	}
 });
 
